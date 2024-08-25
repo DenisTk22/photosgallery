fa7fe76c55cee7bf1264f69b6cb47f021041ce34
Denis Tkachev <allpost-s@ya.ru>
2024-08-25 22:53:01 +0300

    congig

--- src/Components/Jallery/Jallery.jsx
+++ src/Components/Jallery/Jallery.jsx
@@ -1,21 +1,29 @@
+import * as url from 'url';
+import React, { Component } from 'react';
+import { observer } from 'mobx-react';
+import { observable, action, makeObservable, computed, configure } from 'mobx';
+import axios from 'axios';
 import Card from '../Card/Card';
 import { photos } from '../../photos';
 
 import './jallery.css';
 
-function Jallery() {
+configure({ enforceActions: 'observed' });
 
-    // const getPhotos = (url) => {
-    //     fetch(url) // запрос фото с сервера
-    //         .then((response) => {
-    //             if (!response.ok) {
-    //                 throw new Error('Запрос от сервера не успешен')
-    //             }
-    //             return response.json(); 
-    //         })
-    //         .then((data) => displayPhotos(data))
-    //         .catch(console.error('Ошибка данных'))
-    // }
+// @observer 
+export default class Jallery extends Component {
+
+//    photos = [];
+
+    photoSelected = (photo) => {
+        console.log('photo', photo.src);
+        return (
+            <div className='selectedPhoto'>
+                {photo}
+            </div>
+        )
+    }
+
 
     // const displayPhotos = (data) => {
     //     data.map((url) => (
@@ -23,13 +31,41 @@ function Jallery() {
     //     ))
     // }
 
-    return (
-        <main className='jallery'>
-            {photos.map((photo) => (
-                <Card urlPhoto={photo} />
-            ))}
-        </main>
-    )
-}
+    
 
-export default Jallery;
+    render() {
+    // const getPhotosUnicNumber = (url) => {
+    //     fetch(url) // запрос фото с сервера
+    //         .then((response) => {
+    //             if (!response.ok) {
+    //                 throw new Error('Запрос от сервера не успешен')
+    //             }
+    //             return response.json(); 
+    //         })
+    //         // .then((data) => displayPhotos(data))
+    //         .catch(console.error('Ошибка при получении данных'))
+    // }
+
+    //     console.log(getPhotosUnicNumber('http://localhost:8055/items/photosgallery'));
+
+        const getPhotosUnicNumber = async () => {
+            try {
+                const response = await axios.get('http://localhost:8055/items/photosgallery');
+                return response;
+            } catch (error) {
+                console.error('Ошибка при получении данных', error);
+            }
+        }
+
+        console.log(getPhotosUnicNumber());
+
+        return (
+            <main className='jallery'>
+                {photos.map((photo) => (
+                    <Card key={photo} urlPhoto={photo} onClick={() => this.photoSelected(photo)} />
+                ))}
+            </main>
+        )
+    }
+
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import DevTools from 'mobx-react-devtools';
 import Jallery from './Components/Jallery/Jallery'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <>
     <Jallery />
-  </StrictMode>,
+  </>
 )
