b64928a74c5425f854a63b5773032fd56f78b925
Denis Tkachev <allpost-s@ya.ru>
2024-09-08 20:54:45 +0300

    cors settings

--- src/Components/Jallery/Jallery.jsx
+++ src/Components/Jallery/Jallery.jsx
@@ -6,22 +6,22 @@ import Card from '../Card/Card';
 
 const Jallery = () => {
 
-    const url = 'http://localhost:8055/items/photosgallery/1'
+    const url = 'http://localhost:8055/items/photosgallery'
 
-        fetch(url) // запрос фото с сервера
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error('Запрос от сервера не успешен')
-                }
-                return response.json(); 
-            })
-            .then(data => data.json())
-            .then(response => console.log('Ответ', response))
-            .catch(console.error('Ошибка при получении данных'))
+    // fetch(url) // запрос фото с сервера
+    //     .then((response) => {
+    //         if (!response.ok) {
+    //             throw new Error('Запрос от сервера не успешен')
+    //         }
+    //         return response.json(); 
+    //     })
+    //     .then(data => data.json())
+    //     .then(response => console.log('Ответ', response))
+    //     .catch(console.error('Ошибка при получении данных'))
 
-        // axios.get(url)
-        //     .then(response => console.log('Данные>>', response.data))
-        //     .catch(error => console.log('Ошибка>>', error))
+    axios.get(url)
+        .then(response => console.log('Данные>>', response.data))
+        .catch(error => console.log('Ошибка>>', error))
 
     return (
         <main className='jallery'>
